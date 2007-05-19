13029cb4ac460fc2e5388879efa2ea605fa9128b
bender <emilymbender@users.noreply.github.com>
2007-05-19 22:52:50 +0000

    Getting set up for batch translate.

--- gmmt/setup.lisp
+++ gmmt/setup.lisp
@@ -1,10 +1,9 @@
 (in-package :tsdb)
 
 (setf *mmt-languages*
-  '(:hau :isl :zul))
+  '(:arb :eng :epo :fas :fin :hai :hau :heb :hye :isl :ita :mal :spa :zul))
 
 (setf *mmt-transfer-grammars*
   '((:any :isl :isl-acm)
     (:any :hau :hau-acm)
-    (:any :zul :identity)
     (:any :any :identity)))
