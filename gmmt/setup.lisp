b2bc8cdf049812058c04bd9c646079b3ec1cf4f9
bender <emilymbender@users.noreply.github.com>
2007-08-01 18:39:14 +0000

    First draft README file for LOGON distribution of MMT.  Removed arb, spa from setup.lisp.

--- gmmt/setup.lisp
+++ gmmt/setup.lisp
@@ -1,11 +1,10 @@
 (in-package :tsdb)
 
 (setf *mmt-languages*
-  '(:arb :eng :epo :fas :fin :hai :hau :heb :hye :isl :ita :mal :spa :zul))
+  '(:eng :epo :fas :fin :hai :hau :heb :hye :isl :ita :mal :zul))
 
 (setf *mmt-transfer-grammars*
-  '((:any :arb :arb-acm)
-    (:any :eng :eng-acm)
+  '((:any :eng :eng-acm)
     (:any :epo :epo-acm)
     (:any :fas :fas-acm)
     (:any :fin :fin-acm)
@@ -16,6 +15,5 @@
     (:any :isl :isl-acm)
     (:any :ita :ita-acm)
     (:any :mal :mal-acm)
-    (:any :spa :spa-acm)
     (:any :zul :zul-acm)
     (:any :any :identity)))
