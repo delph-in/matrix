de65c9b2678921f74aea5b4b81d2aa10225721ea
bender <emilymbender@users.noreply.github.com>
2007-08-21 04:16:39 +0000

    Various updates to Farsi grammar to model corrected sentences supplied by Nick Pendar.
    Made all transfer grammars use acm.tdl.
    
    Updated Farsi test sentences.
    
    Updated semis to always take prop-or-ques as prop.

--- gmmt/setup.lisp
+++ gmmt/setup.lisp
@@ -1,19 +1,17 @@
 (in-package :tsdb)
 
 (setf *mmt-languages*
-  '(:eng :epo :fas :fin :hai :hau :heb :hye :isl :ita :mal :zul))
+  '(:eng :epo :fas :fin :hau :heb :hye :isl :ita :zul))
 
 (setf *mmt-transfer-grammars*
   '((:any :eng :eng-acm)
     (:any :epo :epo-acm)
     (:any :fas :fas-acm)
     (:any :fin :fin-acm)
-    (:any :hai :hai-acm)
     (:any :hau :hau-acm)
     (:any :heb :heb-acm)
     (:any :hye :hye-acm)    
     (:any :isl :isl-acm)
     (:any :ita :ita-acm)
-    (:any :mal :mal-acm)
     (:any :zul :zul-acm)
     (:any :any :identity)))
