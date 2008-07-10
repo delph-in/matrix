a7749fbb53266ed1c585543a5905e4e75435b426
bender <emilymbender@users.noreply.github.com>
2008-07-10 16:27:51 +0000

    Index.lisp and Relations files for unit test skeletons directory.

---
+++ customize/unit-tests/skeletons/Index.lisp
@@ -0,0 +1,27 @@
+;;;
+;;; this file should be `Index.lisp' and reside in the directory containing the
+;;; tsdb(1) test suite skeleton databases (typically a subdirectory `skeletons'
+;;; in the tsdb(1) database root directory `*tsdb-home*').
+;;;
+;;; the file should contain a single un-quote()d Common-Lisp list enumerating
+;;; the available skeletons, e.g.
+;;;
+;;;   (((:path . "english") (:content . "English TSNLP test suite"))
+;;;    ((:path . "csli") (:content . "CSLI (ERGO) test suite"))
+;;;    ((:path . "vm") (:content . "English VerbMobil data")))
+;;;
+;;; where the individual entries are assoc() lists with at least two elements:
+;;;
+;;;   - :path --- the (relative) directory name containing the skeleton;
+;;;   - :content --- a descriptive comment.
+;;;
+;;; the order of entries is irrelevant as the `tsdb :skeletons' command sorts
+;;; the list lexicographically before output.
+;;;
+
+(
+ ;;; new-unit-test-here
+((:path . "unit-test-10") (:content . "unit-test-10: yet another toy example"))
+((:path . "unit-test-9") (:content . "another toy example"))
+((:path . "unit-test-2") (:content . "toy example"))
+ )
