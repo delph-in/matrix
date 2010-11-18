f49f16f561d1bcfaca5eccf828579ec91c9119d9
goodmami <goodmami@users.noreply.github.com>
2010-11-18 21:06:11 +0000

    Forgot a case of an inline conditional.

--- matrix.py
+++ matrix.py
@@ -24,11 +24,17 @@ def main():
       printhelp()
   try:
     if args[0] in ('c', 'customize'):
-      dest = args[2] if len(args) > 2 else None
+      if len(args) > 2:
+        dest = args[2]
+      else:
+        dest = None
       customize_grammar(args[1], destination=dest)
 
     elif args[0] in ('cf', 'customize-and-flop'):
-      dest = args[2] if len(args) > 2 else None
+      if len(args) > 2:
+        dest = args[2]
+      else:
+        dest = None
       customize_grammar(args[1], destination=dest, flop=True)
 
     elif args[0] in ('v', 'validate'):
