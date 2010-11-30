ce485a9cb085cd54a2fcd36d40a8a675e6b189a4
antske <antske@users.noreply.github.com>
2010-11-30 08:36:32 +0000

    removing old matrix.py

--- auxiliaryComp/matrix.py
+++ auxiliaryComp/matrix.py
@@ -23,74 +23,111 @@ def main():
     elif o in ('-h', '--help'):
       printhelp()
 
+  validate_args(args)
+
   if args[0] in ('c', 'customize'):
     dest = args[2] if len(args) > 2 else None
     customize_grammar(args[1], destination=dest)
-  try:
-    if args[0] in ('c', 'customize'):
-      dest = args[2] if len(args) > 2 else None
-      customize_grammar(args[1], destination=dest)
 
-    elif args[0] in ('cf', 'customize-and-flop'):
-      dest = args[2] if len(args) > 2 else None
-      customize_grammar(args[1], destination=dest, flop=True)
+  elif args[0] in ('cf', 'customize-and-flop'):
+    dest = args[2] if len(args) > 2 else None
+    customize_grammar(args[1], destination=dest, flop=True)
 
-    elif args[0] in ('v', 'validate'):
-      choices_file = os.path.join(args[1], 'choices')
-      if not os.path.exists(choices_file):
-        sys.exit("Error: Choices file not found at " + choices_file)
-      import gmcs.validate
-      v = gmcs.validate.validate_choices(choices_file)
-      for x in v.errors:
-        print x
-        print '  ', v.errors[x]
+  elif args[0] in ('v', 'validate'):
+    choices_file = args[1]
+    if os.path.isdir(choices_file):
+      choices_file = os.path.join(choices_file, 'choices')
+    if not os.path.exists(choices_file):
+      sys.exit("Error: Choices file not found at " + choices_file)
+    import gmcs.validate
+    v = gmcs.validate.validate_choices(choices_file)
+    for x in v.errors:
+      print x
+      print '  ', v.errors[x]
 
-    elif args[0] in ('u', 'utest', 'unit-test'):
-      run_unit_tests()
+  elif args[0] in ('u', 'utest', 'unit-test'):
+    run_unit_tests()
 
-    elif args[0] in ('r', 'rtest', 'regression-test'):
-      cmd = os.path.join(os.environ['CUSTOMIZATIONROOT'],
-                         'regression_tests/run_regression_tests.sh')
-      #Using subprocess makes it difficult to kill the process
-      # (e.g. with Ctrl-C), so we need to handle KeyboardInterrupts
-      # (or alternatively use a os.exec* function)
-      try:
-        p = subprocess.Popen([cmd] + args[1:], env=os.environ)
-        p.wait()
-      except KeyboardInterrupt:
-        print "\nProcess interrupted. Aborting regression tests.\n"
-        import signal
-        os.kill(p.pid, signal.SIGKILL)
+  elif args[0] in ('r', 'rtest', 'regression-test'):
+    cmd = os.path.join(os.environ['CUSTOMIZATIONROOT'],
+                       'regression_tests/run_regression_tests.sh')
+    #Using subprocess makes it difficult to kill the process
+    # (e.g. with Ctrl-C), so we need to handle KeyboardInterrupts
+    # (or alternatively use a os.exec* function)
+    try:
+      p = subprocess.Popen([cmd] + args[1:], env=os.environ)
+      p.wait()
+    except KeyboardInterrupt:
+      print "\nProcess interrupted. Aborting regression tests.\n"
+      import signal
+      os.kill(p.pid, signal.SIGKILL)
 
-    elif args[0] in ('a', 'rtest-add', 'regression-test-add'):
-      choices = args[1]
-      txtsuite = args[2]
-      import gmcs.regression_tests.add_regression_test
-      gmcs.regression_tests.add_regression_test.add(choices, txtsuite)
+  elif args[0] in ('a', 'rtest-add', 'regression-test-add'):
+    choices = args[1]
+    txtsuite = args[2]
+    import gmcs.regression_tests.add_regression_test
+    gmcs.regression_tests.add_regression_test.add(choices, txtsuite)
 
-    elif args[0] in ('i', 'install'):
-      cmd = os.path.join(os.environ['CUSTOMIZATIONROOT'], '../install')
-      location = args[1]
-      # Installations to the live site require validations, so abort.
-      if location.strip('/') == 'matrix/customize':
-        print "Error: For installation to the live site, please use:"
-        print "  matrix.py vivify"
-        sys.exit(2)
-      subprocess.call([cmd, '-r', '-m', location], env=os.environ)
+  elif args[0] in ('i', 'install'):
+    cmd = os.path.join(os.environ['CUSTOMIZATIONROOT'], '../install')
+    location = args[1]
+    # Installations to the live site require validations, so abort.
+    if location.strip('/') == 'matrix/customize':
+      print "Error: For installation to the live site, please use:"
+      print "  matrix.py vivify"
+      sys.exit(2)
+    subprocess.call([cmd, '-r', '-m', location], env=os.environ)
 
-    elif args[0] == 'vivify':
-      # pass the force flag in case the user wants to avoid checks
-      vivify(force)
+  elif args[0] == 'vivify':
+    # pass the force flag in case the user wants to avoid checks
+    vivify(force)
 
-    else:
-      usage()
-
-  except IndexError:
+  else:
     usage()
 
+def validate_args(args):
+  """
+  Run some quick tests to make sure we have the right number of arguments.
+  """
+  if len(args) == 0: usage()
+  elif args[0] in ('c', 'customize'):
+    if len(args) < 2: usage('customize')
+  elif args[0] in ('cf', 'customize-and-flop'):
+    if len(args) < 2: usage('customize-and-flop')
+  elif args[0] in ('v', 'validate'):
+    if len(args) < 2: usage('validate')
+  elif args[0] in ('u', 'utest', 'unit-test'):
+    pass # no other arguments needed
+  elif args[0] in ('r', 'rtest', 'regression-test'):
+    pass # other arguments are optional
+  elif args[0] in ('a', 'rtest-add', 'regression-test-add'):
+    if len(args) < 3: usage('regression-test-add')
+  elif args[0] in ('i', 'install'):
+    if len(args) < 2: usage('install')
+  elif args[0] == 'vivify':
+    pass # no other arguments needed
 
-def usage():
-  print "Usage: matrix.py [OPTION] COMMAND [ARGS...]"
+def usage(command=None):
+  if not command:
+    print "Usage: matrix.py [OPTION] COMMAND [ARGS...]"
+  elif command in ('customize', 'customize-and-flop'):
+    print "Usage: matrix.py [OPTION] " + command + " PATH [DEST]"
+    print "       Where PATH is the path to a choices file or a directory"
+    print "       containing a choices file, and the optional argument DEST"
+    print "       points to the output directory."
+  elif command == 'validate':
+    print "Usage: matrix.py [OPTION] validate PATH"
+    print "       Where PATH is the path to a choices file or a directory"
+    print "       containing a choices file."
+  elif command == 'regression-test-add':
+    print "Usage: matrix.py [OPTION] regression-test-add CHOICES TXTSUITE"
+    print "       Where CHOICES is the path to a choices file and TXTSUITE"
+    print "       is the path to a text file containing test sentences."
+  elif command == 'install':
+    print "Usage: matrix.py [OPTION] install PATH"
+    print "       Where PATH is the path where the Matrix Customization"
+    print "       System should be installed."
+
   print "Try `matrix.py --help' for more information."
   sys.exit(2)
 
@@ -104,7 +141,8 @@ OPTIONS:
 COMMANDS:
     customize (c) PATH [DEST]      : Customize the choices file at PATH,
                                      with the output going to DEST (if
-                                     specified) or PATH.
+                                     specified) or PATH. PATH is either a
+                                     directory or a choices file.
     customize-and-flop (cf) PATH [DEST]
                                    : Customize the choices file at PATH,
                                      then flop the resulting grammar.
@@ -142,7 +180,7 @@ def verify_force():
   print "   Aborted."
   sys.exit(1)
 
-def customize_grammar(directory, destination=None, flop=False):
+def customize_grammar(path, destination=None, flop=False):
   """
   Customize a grammar for the choices file at directory, and if flop
   is True, run flop on the resulting lang-pet.tdl file in the grammar
@@ -153,19 +191,21 @@ def customize_grammar(directory, destination=None, flop=False):
     sys.exit('Error: Cannot flop grammar if LOGONROOT is not set.')
   # customize if choices file found
   import gmcs.customize
-  choices_file = os.path.join(directory, 'choices')
-  if not os.path.exists(choices_file):
-    sys.exit("Error: No choices file found at " + directory)
-  grammar_dir = gmcs.customize.customize_matrix(directory, 'tgz', destination)
+  if os.path.isdir(path):
+    path = os.path.join(path, 'choices')
+  if not os.path.exists(path):
+    sys.exit("Error: No choices file found at " + path)
+  grammar_dir = gmcs.customize.customize_matrix(path, 'tgz', destination)
   # Now a grammar has been created, so we can flop it
   if flop:
     import gmcs.choices
-    lang = gmcs.choices.get_choice('language', choices_file)
+    lang = gmcs.choices.get_choice('language', path)
     pet_file = lang.lower() + '-pet.tdl'
     if not os.path.exists(os.path.join(grammar_dir, pet_file)):
       sys.exit("Error: " + pet_file + " not found.")
     cmd = os.path.join(os.environ['LOGONROOT'], 'bin/flop')
-    subprocess.call([cmd, pet_file], cwd=grammar_dir, env=os.environ)
+    devnull = open('/dev/null', 'w')
+    subprocess.call([cmd, pet_file], cwd=grammar_dir, env=os.environ, stderr=devnull)
 
 
 def run_unit_tests():
