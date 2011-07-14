fffed95839ee398e33712ade1970b3b53da7e34f
antske <antske@users.noreply.github.com>
2011-07-14 16:34:22 +0000

    further development of tdl checking code

--- germanic-languages/gmcs/tdlcheck.py
+++ germanic-languages/gmcs/tdlcheck.py
@@ -7,8 +7,12 @@ import shutil
 from gmcs import tdl
 from gmcs.tdl import TDLparse
 from gmcs.tdl import TDLelem_type
+from gmcs.tdl import TDLelem_av
+from gmcs.tdl import TDLelem_conj
+from gmcs.tdl import TDLelem_feat
+from gmcs.tdl import TDLelem_coref
+from gmcs.tdl import TDLelem_dlist
 from gmcs.tdl import TDLset_file
-
 
 
 class TDLdefined_type(object):
@@ -18,6 +22,8 @@ class TDLdefined_type(object):
     self.op = op
     self.supertypes = []
     self.subtypes = []
+    self.values = []
+    self.attributes = []
     self.val_constr = {}
     self.coind_constr = {}
     
@@ -39,26 +45,52 @@ class TDLdefined_type(object):
   def add_subtype(self, type_name):
     self.subtypes.append(type_name)
 
+  def add_value(self, val_name):
+    if not val_name == None:
+      if not val_name in self.values:
+        self.values.append(val_name)   
 
-def create_type_dict_from_tdl(file, type_dict):
-  print file.name
+  def add_attribute(self, attr_name):
+    if not attr_name in self.attributes:
+      self.attributes.append(attr_name)
+
+  def add_att_val_pair(self, att, v):
+    if att in self.val_constr:
+      new_val = self.val_constr[att] + " & " + v
+      self.val_constr.update([[att, new_val]])
+    else:
+      self.val_constr.update([[att, v]])
+
+  #p is set of paths leading to marked up co-reference
+  #coref is symbol used to mark coreference
+  def add_coref_constraint(self, coref, p):
+    if coref in self.coind_constr:
+      old_p = self.coind_constr[coref]
+      my_ps = old_p | p
+    else:
+      my_ps = p
+    self.coind_constr.update([[coref, my_ps]])
+
+def partOfComment(line, flag):
+  if re.match('\s*\#\|', line):
+            #multiline comment starts
+    flag = 1
+  elif re.match('.*\|\#', line):
+    flag = 0
+
+  return flag
+
+
+def create_type_dict_from_tdl(file, type_dict, att):
   lines = file.readlines()
   file.close()
   temp_descr = ''
   flag = 0
   for line in lines:
-    if re.match('\s*\#\|', line):
-            #multiline comment starts
-      flag = 1
-    elif re.match('.*\|\#', line):
-      flag = 0
+    flag = partOfComment(line, flag)
 
     if not ((re.match('\s*;', line)) or (flag == 1)):
-
-#TODO in the following line I ignore comments 
-#appearing in a description itself
-#I do not see any good way to store and 
-#reproduce them later 
+#Ignoring comments, okay at this stage: just processing the types
       line = re.sub( r';.*', '', line )
       temp_descr += line
 #search for potential end of description
@@ -67,9 +99,9 @@ def create_type_dict_from_tdl(file, type_dict):
         temp_descr = ''
         tdl_object = TDLparse(description)
         type_dict[tdl_object.type] = tdl_object      
-  
+        collect_attributes(tdl_object, att)
 
-def identify_inst_types(itypes, deftypes):
+def identify_inst_types(itypes, deftypes, attributes):
   #set of elementary types requested by instantiated types
   req_inst_types = set()
   #adding 'null': not often specified, but generally required
@@ -79,52 +111,218 @@ def identify_inst_types(itypes, deftypes):
   id_inst_types = set()
 
   #retrieve all requested elem types from instantiated types
-  for k, v in itypes.iteritems():
-    identify_objects(v.child, req_inst_types)
+  for v in itypes.itervalues():
+    identify_objects(v.child, req_inst_types, attributes)
   
-  walk_through_instantiated(req_inst_types, id_inst_types, deftypes)
+  walk_through_instantiated(req_inst_types, id_inst_types, deftypes, attributes)
   
 
   return id_inst_types
 
 
-def walk_through_instantiated(requested, identified, defined):
+###TO DO: add subtype values at correct position
+###plan: define attribute set of values on TDLdefined_type
+###use typhierarchy for defined stuff
+###collect supertypes for id-ing new subtypes
+###should be done with complete hierarchy: else old problem will occur: types with relevant definitions 
+###will not be marked as subtypes...
+
+def walk_through_instantiated(requested, identified, defined, atts):
   new_rit = set()
- #dangerous: operating on set that is looped through...
+
   for rit in requested:
     if rit in defined:
       identified.add(rit)
-      temp_rit = set()
-      identify_objects(defined[rit].child, temp_rit)
+#attributes in defined type:
+      for a in defined[rit].attributes:
+        atts.add(a)
+#create list of defined type's value
+      my_types = defined[rit].values
+#for supertypes of defined type: add current type as subtype
+#add to my_types list
+      for s in defined[rit].supertypes:
+        if s in defined:
+          defined[s].add_subtype(rit)
+        else:
+          print s + ' listed as supertype not found in hierarchy'
+        my_types.append(s)
+
+      temp_rit = set(my_types)
+         
+  #    identify_objects(defined[rit].child, temp_rit, attributes)
       for nt in temp_rit:
         if not (nt in identified or nt in requested):
           new_rit.add(nt)
       temp_rit.clear()
     else:
-      print "Problem: somehow the following type is requested but not defined:"
-      print rit
+      if not rit == '*top*':
+        print "Problem: somehow the following type is requested but not defined:"
+        print rit
   if len(new_rit) > 0:
-    walk_through_instantiated(new_rit, identified, defined)
+    walk_through_instantiated(new_rit, identified, defined, atts)
 
-def identify_objects(tdl_e_c_c, itypes):
+'interprets children of TDLelem_typedef and adds them to object of type TDLdefined_type'
+def convert_elem_type_to_type_def(elem, typedef):
+  for ch in elem.child:
+#check for not embedded elementary types: these are supertypes
+    if isinstance(ch, TDLelem_type):
+      st = process_elementary_type(ch)
+      typedef.add_supertype(st)
+#call recursive function for all other children
+    else:
+      interpret_elemtype_children(ch, typedef)
+
+def interpret_elemtype_children(elem, typedef):
+  if isinstance(elem, TDLelem_av):
+    path = ''
+    process_att_val_type(elem, path, typedef)
+  elif isinstance(elem, TDLelem_feat) or isinstance(elem, TDLelem_conj):
+    for ch in elem.child:
+      interpret_elemtype_children(ch, typedef)    
+  elif not elem == None:  #TDLelem_feat and conj are covered by this...always has children of type av, no own attributes
+    print "New case scenario: following type occurring unembedded: "
+    print elem
+
+def identify_objects(tdl_e_c_c, itypes, attrs):
   for ch in tdl_e_c_c:
     if isinstance(ch, TDLelem_type):
-      process_elementary_type(ch, itypes)
-    identify_objects(ch.child, itypes)
+      my_type = process_elementary_type(ch)
+      if not (my_type == None):
+        itypes.add(my_type)
+    elif isinstance(ch, TDLelem_av):
+      attrs.add(ch.attr)
+    identify_objects(ch.child, itypes, attrs)
+
+def collect_attributes(t, att):
+  for ch in t.child:
+    if isinstance(ch, TDLelem_av):
+      att.add(ch.attr)
+    if not ch == None:
+      collect_attributes(ch, att)
 
 
+def process_att_val_type(t, p, td):
+  td.add_attribute(t.attr)
+  p += t.attr
+  if len(t.child) == 0:
+    print "this attribute thing has no children"
+  elif len(t.child) == 1 and isinstance(t.child[0], TDLelem_conj):
+    t = t.child[0]
+    if len(t.child) == 1:
+      t = t.child[0]
+      interpret_av_emb_child(t, p, td)
+    else:
+      for c in t.child:
+        temp_p = p
+        interpret_av_emb_child(c, temp_p, td)
+  else:
+    print "av type has unusual child or number of children: "
+    print t.child[0]
+    print len(t.child)
+    
 
-def process_elementary_type(t, tset):
-  out = open('check_file.txt', 'a')
+def interpret_av_emb_child(t, p, td):
+  if isinstance(t, TDLelem_feat):
+    interpret_embedded_feat(t, p, td)
+      
+  elif isinstance(t, TDLelem_type):
+    my_val = process_elementary_type(t)
+    td.add_value(my_val)
+    td.add_att_val_pair(p, my_val)
+  elif isinstance(t, TDLelem_coref):
+    if len(t.child) > 0:
+      print "Problem: coreference type occurring with children"
+    else:
+      my_path = set([p])
+      td.add_coref_constraint(t, my_path)
+  elif isinstance(t, TDLelem_dlist):
+    interpret_dlist(t, p, td)
+  else:
+    if not t is None:
+      print "New case scenario: av_type has child:"
+      print t
+
+def interpret_embedded_feat(t, p, td):
+  if len(t.child) == 1:
+    t = t.child[0]
+    if isinstance(t, TDLelem_av):
+      p += '.'
+      process_att_val_type(t, p, td)
+    else:
+      print 'Problem: feature has non-attribute value child...'
+  elif len(t.child) > 1:
+    for ch in t.child:  
+      temp_p = p    
+      if isinstance(ch, TDLelem_av):
+        temp_p += '.'
+        process_att_val_type(ch, temp_p, td)
+      else:
+        print 'Problem: feature has non-av child'
+
+###to do: not completely done yet: does not interpret complex a_v within dlist value
+###not too relevant for current purposes, so leaving this for now
+###Also todo: deal with complex structures
+def interpret_dlist(t, p, td):
+  val = ''
+  if len(t.child) == 0:
+    val = '<! !>'
+  elif len(t.child) == 1:
+    t = t.child[0]
+    for tch in t.child:
+      if isinstance(tch, TDLelem_type):
+        temp_val = process_elementary_type(tch)
+        td.add_value(temp_val)
+        val = '<! ' + temp_val + ' !>'
+      elif isinstance(tch, TDLelem_feat):
+        if len(tch.child) == 1:
+          t = tch.child[0]
+          if isinstance(t, TDLelem_av):
+            td.add_attribute(t.attr)
+    #ToDo: make complete (but is a detail for now...)
+            val = '<! [ ' + t.attr + ' ]!>'
+        else:
+            val = '<! [ ] !>'
+  #        for myc in tch.child:
+  #          print myc.attr
+  else:
+    val = '<! [ ] !>'
+  if val:
+    td.add_att_val_pair(p, val)
+
+
+def write_dotted_path(c, p):
+  if len(c.child) == 1 and isinstance(c.child[0], TDLelem_conj):
+    c = c.child[0]
+    if len(c.child) == 1 and isinstance(c.child[0], TDLelem_feat):
+      c = c.child[0]
+      if len(c.child) >= 1 and isinstance(c.child[0], TDLelem_av):
+        c = c.child[0]
+        p += c.attr 
+        p += "."
+        produce_attribute_path(c, p)
+        return True
+  return False
+
+def produce_attribute_path(c, p):
+
+  if not write_dotted_path(c, p):
+    if isinstance(c, TDLelem_av):
+      p += c.attr
+      for ch in c.child:
+        produce_attribute_path(ch, p)
+
+def process_elementary_type(t):
+#for loop is safety check: elementary types should not have children
+  for ch in t.child:
+    print "elementary type child: "
+    print ch
+    if isinstance(ch, TDLelem_type):
+      process_elementary_type(ch)
+    else:
+      print "more work needed here...."
   if not re.match('\"*\"',t.type):
     if not t.type == 'null':
-      tset.add(t.type)
-      out.write(t.type + '\n')
-  for ch in t.child:
-    print "type child: "
-    print ch
-    identify_objects(ch)
-
+      return t.type
 
 
 def separate_instantiated_non_instantiated(file, instset, sfset):
@@ -140,18 +338,16 @@ def separate_instantiated_non_instantiated(file, instset, sfset):
   output = open(file.name, 'w')
   dump_output = open(unused_types, 'w')
   for line in lines:
-    if re.match('\s*\#\|', line):
-            #multiline comment starts
-      flag = 1
-    elif re.match('.*\|\#', line):
-      flag = 0
+    
+    flag = partOfComment(line, flag)
 
     if not ((re.match('\s*;', line)) or (flag == 1)):
 
 #TODO in the following line I ignore comments 
 #appearing in a description itself
-#I do not see any good way to store and 
-#reproduce them later 
+#INSTEAD: use groups before ';' and after
+#before is line, after is stored as comment
+
       line = re.sub( r';.*', '', line )
       temp_descr += line
 #search for potential end of description
@@ -175,25 +371,92 @@ def separate_instantiated_non_instantiated(file, instset, sfset):
   dump_output.close()
   check.close()
 
+
+def create_type_inventory(file, deftypes):
+
+  lines = file.readlines()
+  file.close()
+  temp_descr = ''
+  flag = 0
+  for line in lines:
+    flag = partOfComment(line, flag)
+
+    if not ((re.match('\s*;', line)) or (flag == 1)):
+#Ignoring comments, okay at this stage: just processing the types
+      line = re.sub( r';.*', '', line )
+      temp_descr += line
+#search for potential end of description
+      if re.search('\.\s*\n',  line):
+        description = temp_descr
+        temp_descr = ''
+        tdl_object = TDLparse(description)
+        tdldef = TDLdefined_type(tdl_object.type, tdl_object.op)
+        #parse tdl_object to obtain further info on type
+        if len(tdl_object.child) == 1:
+          t = tdl_object.child[0]
+          convert_elem_type_to_type_def(t, tdldef)
+          deftypes[tdldef.type] = tdldef   
+        else:
+          print "Problem: general typedef has more than one child"
+
+
+def build_defined_types_hierarchy(path, type_defs):
+  deftypes = {}
+  for f in type_defs:
+    file = open( path + '/' + f)
+    create_type_inventory(file, deftypes)
+    file.close()
+  
+####depending on structure: change name of function...
 def process_instantiation_files(path, inst, type_defs):
+
+  typehierarchy = {}
+  for dtf in type_defs:
+    file = open ( path + '/' + dtf)
+#    separate_instantiated_non_instantiated(file, instantiatedset, superflset)
+    create_type_inventory(file, typehierarchy)
+    file.close()  
+  print len(typehierarchy)
+
   itypes = {}
+  attributes = set()
   for f in inst:
     file = open( path + '/' + f )
-    create_type_dict_from_tdl(file, itypes) 
+    create_type_dict_from_tdl(file, itypes, attributes) 
     file.close
+  print "number of attributes identified in instiated files: "
+  print len(attributes)
 
-  deftypes = {}
-  for tf in type_defs:
-    file = open( path + '/' + tf )
-    create_type_dict_from_tdl(file, deftypes)
-    file.close()
-
+#  deftypes = {}
+#  allattr = set()
+#  for tf in type_defs:
+#    file = open( path + '/' + tf )
+#    create_type_dict_from_tdl(file, deftypes, allattr)
+#    file.close()
+#  print "number of attributes identified in defined types "
+#  print len(allattr)
   
-  instantiatedset = identify_inst_types(itypes, deftypes)
+  instantiatedset = identify_inst_types(itypes, typehierarchy, attributes)
   superflset = set()
-  for k, v in deftypes.iteritems():
+  for k in typehierarchy.iterkeys():
     if not k in instantiatedset:
       superflset.add(k)
+  print "total attributes on instantiated types: "
+  print len(attributes)
+  
+####TO DO: do the attribute introduction check:
+# 1. *top* types: as initiation types (global list)
+# 2. for each:
+# - check attribute introduction (presence of non-introduced atts)
+# - if identified :-> pop from asked attributes, add to requested if not already present
+y# - check each subtype
+# - if done for all, place subtypes on instance list
+# do while len(attributes) > 0
+#
+# 3. if new requested > 0:
+#  replay came of identifying sub and supertypes
+# 4. repeat if new attributes found...
+
 
   print "identified: " 
   print len(superflset)
@@ -205,16 +468,12 @@ def process_instantiation_files(path, inst, type_defs):
     check.write('\n')
   check.close()
 
-  for dtf in type_defs:
-    file = open ( path + '/' + dtf)
-    separate_instantiated_non_instantiated(file, instantiatedset, superflset)
-    file.close()
 
-  output_file = open( 'output.txt' , 'w')
-  for it in instantiatedset:
-    output_file.write(it)
-
-  output_file.close()
+#  output_file = open( 'output.txt' , 'w')
+#  for it in instantiatedset:
+#    output_file.write(it)
+#
+#  output_file.close()
 #    output_file = open( 'test.tdl' , 'w')
   
 
@@ -266,8 +525,8 @@ def identify_type_def_files(script):
 def process_script(path):
   script = open( path + '/lkb/script' )   
   inst = identify_instantiation_files(script)
-  script.close
+  script.close()
   script = open( path + '/lkb/script' )
   type_defs = identify_type_def_files(script)
-  script.close
+  script.close()
   process_instantiation_files(path, inst, type_defs)
