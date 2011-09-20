ab5cba4eefb381a2dd01a0f20d00ec22cd2a03eb
waxd <waxd>
2011-09-20 13:34:09 +0000

    Uncommented out some code that needs to run.

--- matrix.py
+++ matrix.py
@@ -404,8 +404,8 @@ def run_web_tests():
   except (NameError):
     sys.stderr.write("Seleinum not installed: run \"pip install -U selenium\"\n")
   cmd = os.path.join(os.environ['CUSTOMIZATIONROOT'], '../install')
-#  user_name = raw_input("Patas user name:")
-#  subprocess.call([cmd, '-r', '-a', 'patas.ling.washington.edu', '-u', user_name, 'home2/www-uakari/html/matrix/test'], env=os.environ);
+  user_name = raw_input("Patas user name:")
+  subprocess.call([cmd, '-r', '-a', 'patas.ling.washington.edu', '-u', user_name, 'home2/www-uakari/html/matrix/test'], env=os.environ);
   import unittest
   import gmcs.tests.testWeb
   loader = unittest.defaultTestLoader
