50aca69d74880624d88366ccd4e67772dff7ae9c
antske <antske@users.noreply.github.com>
2011-10-31 13:11:39 +0000

    added minor tests for subject raising with argcomp and auxrule, minor error not of influence on test in choices for argcomp: change if causes problems later

--- gmcs/regression_tests/skeletons/Index.lisp
+++ gmcs/regression_tests/skeletons/Index.lisp
@@ -22,6 +22,8 @@
 (
 ((:path . "tiniest") (:content . "tiniest: A very basic grammar just to get the regression tests started.  SOV word order, no frills."))
  ;;; new-regression-test-here
+((:path . "ger-subjcontr-auxrule") (:content . "ger-subjcontr-auxrule: Small test for subject control verbs with minor word order variations using auxrule analysis"))
+((:path . "ger-subcontr-argcomp") (:content . "ger-subcontr-argcomp: Testing whether subject control works with arg-comp analysis and minor word order variations"))
 ((:path . "german-auxrule-compl") (:content . "german-auxrule-compl: German test suite covering ACL data using auxrule analysis"))
 ((:path . "german-argcomp-compl") (:content . "german-argcomp-compl: German test covering ACL2011 data using arg-comp analysis"))
 ((:path . "dutch-auxrule-compl") (:content . "dutch-auxrule-compl: Dutch regression test using auxrule analysis"))
