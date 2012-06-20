4c998c8dafb0598cbb5dfb2a5e918d14d69ee177
sanghoun <sanghoun@users.noreply.github.com>
2012-06-20 03:59:27 +0000

    sanghoun20120619

--- my_ci.sh
+++ my_ci.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 svn update
+rm -rf gmcs/regression_tests/grammars/*
+rm -rf gmcs/regression_tests/home/current/*
+rm -rf gmcs/regression_tests/logs/*
 find . -name "*.pyc" -exec rm -rf {} \;
 find . -name "*~" -exec rm -rf {} \;
 svn add --force *
