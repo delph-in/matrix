a1bc7caade030b697fb7963b9af023b49103433b
trimblet <dantiston@users.noreply.github.com>
2014-11-03 16:39:35 +0000

    direct inverse test

--- gmcs/linglib/direct_inverse.py
+++ gmcs/linglib/direct_inverse.py
@@ -29,6 +29,7 @@ def get_subj_comps_types(j, scale_size, direc, equal):
 ##########################
 
 def customize_direct_inverse(choices, mylang, hierarchies):
+  sys.stderr.write("CUSTOMIZING DIRECT INVERSE") # TODO: DELETEME
   if not choices.has_dirinv():
       return
   write_dir_inv_types(choices, mylang, hierarchies)
