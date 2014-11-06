f9528fbc8642d6370cdffe3616806662632e667b
trimblet <dantiston@users.noreply.github.com>
2014-11-06 20:17:25 +0000

    fixes to the switching lexical rule types 15

--- gmcs/linglib/morphotactics.py
+++ gmcs/linglib/morphotactics.py
@@ -533,7 +533,7 @@ def percolate_supertypes(pc):
 ######################
 
 def write_rules(pch, mylang, irules, lrules, lextdl, choices):
-  raise Exception([lrt.features for pc in pch.nodes.values for lrt in pc.nodes.values]) # DELETEME
+  raise Exception([lrt.features for pc in pch.nodes.values() for lrt in pc.nodes.values()]) # DELETEME
   all_flags = get_all_flags('out').union(get_all_flags('in'))
   write_inflected_avms(mylang, all_flags)
   mylang.set_section('lexrules')
