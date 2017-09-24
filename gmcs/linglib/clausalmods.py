059ed7e969704b0c5a3b81ad24426e3164691bdb
kphowell <kphowell@users.noreply.github.com>
2017-09-24 19:08:40 +0000

    additions to matrixdef

--- gmcs/linglib/clausalmods.py
+++ gmcs/linglib/clausalmods.py
@@ -101,7 +101,7 @@ def add_subord_lex_item(mylang, pos, subpos):
   mylang.set_section('addenda')
   mylang.add('head :+ [ INIT bool ].')
 
-def add_phrasal_types(mylang, rules, pos, subpos):
+def add_subord_phrasal_types(mylang, rules, pos, subpos):
   """
   Add the phrase type definitions
   """
@@ -122,6 +122,19 @@ def add_phrasal_types(mylang, rules, pos, subpos):
   elif subpos == 'after':
     rules.add('comp-head := comp-head-phrase.')
 
+def add_subordinator_pair_to_lexicon(lexicon, matrixtype, subordtype, pair):
+  """
+  Adds each member of a subordinator pair to the lexicon
+  """
+  if matrixtype == 'adv':
+    lexicon.add(matrixorth + ':= subpair-adv-lex-item &\
+  [ STEM < "' + matrixorth '" >,\
+    SYNSEM.LKEYS.KEYREL.PRED "' + pred '" ].')
+  #elif matrixtype == 'comp'
+
+  if subordtype == 'comp':
+    lexicon.add()
+
 def customize_clausalmods(mylang, ch, lexicon, rules, irules):
   """
   The main clausal modifier customization routine
@@ -133,13 +146,19 @@ def customize_clausalmods(mylang, ch, lexicon, rules, irules):
 
     pos = cms.get('position')
     subord = cms.get('subordinator')
-    subpos = cms.get('subposition')
 
 
     if subord == 'free':
+      subpos = cms.get('subposition')
       for subord in cms.get('freemorph'):
         add_free_subordinator_to_lexicon(lexicon, subord, pos, subpos)
       add_subord_lex_item(mylang, pos, subpos)
-      add_phrasal_types(mylang, rules, pos, subpos)
-    # if subord == 'pair':
-    #   for pair in cms.get('morphpair'):
+      add_subord_phrasal_types(mylang, rules, pos, subpos)
+
+    if subord == 'pair':
+      matrixtype = cms.get('matrixtype')
+      subordtype = cms.gete('subordtype')
+      for pair in cms.get('morphpair'):
+        add subordinator_pair_to_lexicon(lexicon, matrixtype, subordtype, pair)
+      add_pair_lex_items()
+      add_pair_phrasal_types()
