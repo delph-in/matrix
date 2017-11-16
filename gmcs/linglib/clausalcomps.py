757f8ff648234c2262cedf1552515b474d465311
olzama <olzama@users.noreply.github.com>
2017-11-16 18:56:59 +0000

    complementizer test grammar

---
+++ gmcs/linglib/clausalcomps.py
@@ -0,0 +1,10 @@
+from gmcs.utils import TDLencode
+from gmcs.utils import orth_encode
+from gmcs.lib import TDLHierarchy
+
+######################################################################
+# Clausal Complamants
+#   Create the type definitions associated with the user's choices
+#   about clasual complements.
+
+######################################################################
