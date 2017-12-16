3b1c2823dee1844f87a025f1da98eceef99fdb39
kphowell <kphowell@users.noreply.github.com>
2017-12-16 20:35:59 +0000

    added subord-pairs test

--- gmcs/regression_tests/skeletons/Index.lisp
+++ gmcs/regression_tests/skeletons/Index.lisp
@@ -22,6 +22,7 @@
 (
 ((:path . "tiniest") (:content . "tiniest: A very basic grammar just to get the regression tests started.  SOV word order, no frills."))
  ;;; new-regression-test-here
+((:path . "clausalmods-subord-pairs") (:content . "clausalmods-subord-pairs: Clausal modifiers with subordinator pairs (one in the subord and one in the matrix clause)"))
 ((:path . "clausalmods-special-morph") (:content . "clausalmods-special-morph: Clausal modifiers that are marked by special verbal morphology"))
 ((:path . "clausalmods-shared-subject-free-subord") (:content . "clausalmods-shared-subject-free-subord: Clausal modifiers with a free subordinator that share their subject with the matrix verb"))
 ((:path . "clausalmods-shared-subject-bound-subord") (:content . "clausalmods-shared-subject-bound-subord: Clausal modifiers marked by a bound subordinator morpheme that share their subject with the matrix verb"))
