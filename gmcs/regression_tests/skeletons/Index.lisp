95410092ecb0e1aea4c30a064da26a6905c1e52e
kphowell <kphowell@users.noreply.github.com>
2017-12-18 18:53:39 +0000

    added shared subjects

--- gmcs/regression_tests/skeletons/Index.lisp
+++ gmcs/regression_tests/skeletons/Index.lisp
@@ -22,6 +22,8 @@
 (
 ((:path . "tiniest") (:content . "tiniest: A very basic grammar just to get the regression tests started.  SOV word order, no frills."))
  ;;; new-regression-test-here
+((:path . "clausalmods-shared-subject-free-subord") (:content . "clausalmods-shared-subject-free-subord: Clausal modifiers marked wiht a free subordinator morpheme that share a subject with the matrix verb"))
+((:path . "clausalmods-shared-subject-bound-subord") (:content . "clausalmods-shared-subject-bound-subord: Clausal modifiers marked by a bound subordinator morpheme that share their subject with the matrix verb"))
 ((:path . "clausalmods-morph-and-subord") (:content . "clausalmods-morph-and-subord: Clausal modifiers that are marked by a free subordinator morpheme and have special verbal morphology"))
 ((:path . "clausalmods-modifier-position") (:content . "clausalmods-modifier-position: Clausal modifiers that attach before, after or before and after the matrix clause"))
 ((:path . "clausalmods-mod-position") (:content . "clausalmods-mod-position: Clausal mods test"))
