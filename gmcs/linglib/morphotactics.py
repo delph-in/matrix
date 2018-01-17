379e7c5f6542503bfaeb9b7649f6f46e68f4c381
kphowell <kphowell@users.noreply.github.com>
2018-01-17 01:28:53 +0000

    validation change to morphotactics

--- gmcs/linglib/morphotactics.py
+++ gmcs/linglib/morphotactics.py
@@ -1121,13 +1121,14 @@ def lrt_validation(lrt, vr, index_feats, choices, incorp=False, inputs=set(), sw
                             'unusable without a copula defined on the Lexicon page.')
 
     #KPH Validation for case change on nominalization rules
-    for lri in lrt.get('lri', []):
-        if 'case' in lri.get('feat', []):
-            vr.warn(lrt.full_key + '_lri', 'If case change is specified on the object ' + \
-                   'in a nominalization rule, the resulting lexical rule will only be ' + \
-                   'compatible with transitive verbs. If this lexical rule should also ' + \
-                   'be possible for intransitive verbs, create another lexical rule that ' + \
-                    'requires intransitives verbs as the input.')
+    for lri in lrt.get('lri'):
+        for feat in lri.get('feat'):
+            if feat.get('name') == 'case':
+                vr.warn(lrt.full_key + '_lri', 'If case change is specified on the object ' + \
+                       'in a nominalization rule, the resulting lexical rule will only be ' + \
+                       'compatible with transitive verbs. If this lexical rule should also ' + \
+                       'be possible for intransitive verbs, create another lexical rule that ' + \
+                        'requires intransitives verbs as the input.')
 
 def hierarchy_validation(choices, pc, vr):
     # LLD 2015-11-22 Supertype LRTs should not have non-affixing LRIs if subtype
