a50c864b7f02870cc7887e50228381eaabd3e2b1
ekayen <ekayen@users.noreply.github.com>
2018-01-22 03:04:26 +0000

    added new test

--- gmcs/regression_tests/skeletons/Index.lisp
+++ gmcs/regression_tests/skeletons/Index.lisp
@@ -22,6 +22,7 @@
 (
 ((:path . "tiniest") (:content . "tiniest: A very basic grammar just to get the regression tests started.  SOV word order, no frills."))
  ;;; new-regression-test-here
+((:path . "adnom-poss-spec-dep-non-aff-free-wo") (:content . "adnom-poss-spec-dep-non-aff-free-wo: Dep non-affixally marked free-wo poss construction w spec-like attachment"))
 ((:path . "adnom-poss-spec-dep-non-aff-hf") (:content . "adnom-poss-spec-dep-non-aff-hf: Dep non-affixally marked head final poss construction w spec-like attachment"))
 ((:path . "adnom-poss-spec-dep-aff-head-non-aff-hf-agr-head") (:content . "adnom-poss-spec-dep-aff-head-non-aff-hf-agr-head: Dep affixally marked, head non-affixally, dead agreeing, head final poss construction w spec-like attachment"))
 ((:path . "adnom-poss-spec-dep-aff-head-non-aff-hf-agr-dep") (:content . "adnom-poss-spec-dep-aff-head-non-aff-hf-agr-dep: Dep affixally marked, head non-affixally, dep agreeing, head final poss construction w spec-like attachment"))
--- reg-tests/renamed/adnom-poss-spec-dep-non-aff-free-wo/adnom-poss-spec-dep-non-aff-free-wo/Version.lsp
+++ reg-tests/renamed/adnom-poss-spec-dep-non-aff-free-wo/adnom-poss-spec-dep-non-aff-free-wo/Version.lsp
@@ -1,4 +1,4 @@
 ;;; -*- Mode: TDL; Coding: utf-8 -*-
 (in-package :common-lisp-user)
 
-(defparameter *grammar-version* "adnom-poss-spec-dep-non-aff-free-wo (2018-01-21_06:19:02_UTC)")
+(defparameter *grammar-version* "adnom-poss-spec-dep-non-aff-free-wo (2018-01-22_03:01:02_UTC)")
