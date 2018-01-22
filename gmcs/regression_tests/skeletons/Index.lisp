a45f5ef301f9222e5ed298023baed125445c0be8
ekayen <ekayen@users.noreply.github.com>
2018-01-22 04:24:03 +0000

    added new test

--- gmcs/regression_tests/skeletons/Index.lisp
+++ gmcs/regression_tests/skeletons/Index.lisp
@@ -22,6 +22,7 @@
 (
 ((:path . "tiniest") (:content . "tiniest: A very basic grammar just to get the regression tests started.  SOV word order, no frills."))
  ;;; new-regression-test-here
+((:path . "adnom-poss-spec-dep-non-aff-head-non-aff-free-wo") (:content . "adnom-poss-spec-dep-non-aff-head-non-aff-free-wo: Dep and head non-affixally marked, free-wo poss construction w spec-like attachment"))
 ((:path . "adnom-poss-spec-dep-non-aff-head-non-aff-hf") (:content . "adnom-poss-spec-dep-non-aff-head-non-aff-hf: Dep and head non-affixally marked, head final poss construction w spec-like attachment"))
 ((:path . "adnom-poss-spec-dep-non-aff-head-aff-hf-agr-head") (:content . "adnom-poss-spec-dep-non-aff-head-aff-hf-agr-head: Dep non-affixally marked, head affixally, head agreeing, head final poss construction w spec-like attachment"))
 ((:path . "adnom-poss-spec-dep-non-aff-head-aff-hf-agr-dep") (:content . "adnom-poss-spec-dep-non-aff-head-aff-hf-agr-dep: Dep non-affixally marked, head affixally, dep agreeing, head final poss construction w spec-like attachment"))
