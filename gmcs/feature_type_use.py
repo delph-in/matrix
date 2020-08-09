3a18f67e9f60d8af907b43f2a18ef8c1accac886
Olga Zamaraeva <olzama@uw.edu>
2020-08-09 16:28:39 -0700

    no message

---
+++ gmcs/feature_type_use.py
@@ -0,0 +1,7 @@
+# Attempt to flag specific features which were added to the grammar by a component.
+# One such feature is INIT; it is not needed in all grammars but can be added by the Word Order library,
+# the clausal complements library, the clausal modifiers library, and the Wh-questions library
+# (and possibly other libraries).
+
+USED_FEATURES = {'INIT':False}
+USED_TYPES = {'qdet':False,'qpro':False,'qadv':False,'qverb':False}
