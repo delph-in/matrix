65b1d7ef6afb2c260ff84a3448420708e27f5554
Olga Zamaraeva <olzama@uw.edu>
2020-08-09 15:14:09 -0700

    no message

---
+++ gmcs/linglib/adverbs_adpositions.py
@@ -0,0 +1,31 @@
+'''
+This is not a fully fledged library.
+Below is what was added along with the Constituent (Wh-) Questions library, mostly for testing purposes.
+olzama@uw.edu
+'''
+
+from gmcs.utils import get_name,TDLencode, orth_encode
+
+from gmcs import constants
+
+# Constants
+
+HEAD_ADJ = '''my-head-adj-phrase := head-adj-int-phrase &
+ [ HEAD-DTR.SYNSEM.LOCAL.CAT [ HEAD +nvr, VAL [ SUBJ clist, COMPS clist ] ],
+   NON-HEAD-DTR.SYNSEM.LOCAL.CAT [ HEAD +jrp, VAL.COMPS < > ] ].
+ '''
+
+ADJ_HEAD = '''my-adj-head-phrase := adj-head-int-phrase &
+ [ HEAD-DTR.SYNSEM [ LOCAL.CAT [ HEAD +nvr ] ],
+   NON-HEAD-DTR.SYNSEM.LOCAL.CAT [ HEAD +jrp, VAL.COMPS < > ] ].
+ '''
+
+def customize_adv_adp(ch, mylang, rules):
+    if len(ch.get('adv',[])) > 0 or len (ch.get('normadp',[])) > 0: #need to handle also adjectives here
+        mylang.add_literal(';;; Head Adjunct rules',section='phrases')
+        mylang.add_literal('; For intersective adjuncts with underspecified attachment locations:',section='phrases')
+        mylang.add(HEAD_ADJ,section='phrases')
+        mylang.add(ADJ_HEAD,section='phrases')
+        mylang.add('bare-np-phrase := [ SYNSEM.LIGHT - ].')
+        rules.add('head-adj := my-head-adj-phrase.')
+        rules.add('adj-head := my-adj-head-phrase.')
