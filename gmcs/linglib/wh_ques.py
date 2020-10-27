ed92e6f6ebc0de25436b5fdc79bd5931d06029be
Olga Zamaraeva <olzama@uw.edu>
2020-10-27 13:01:50 -0700

    Constraint not needed.

--- gmcs/linglib/wh_ques.py
+++ gmcs/linglib/wh_ques.py
@@ -272,10 +272,7 @@ def customize_wh_ques(mylang, ch, rules, roots):
         if ch.get(MTRX_FRONT) in [SINGLE, MULTI]:
             mylang.add('insitu-int-cl := [ HEAD-DTR.SYNSEM.L-QUE - ].')
 
-        if (ch.get(MTRX_FRONT) == SINGLE
-                and not ch.get(EMBED_INSITU) == ON):
-            mylang.add('insitu-int-cl := [ SYNSEM.LOCAL.CAT.MC + ].')
-        elif ch.get(MTRX_FRONT) == 'multi' and ch.get(MTRX_FR_OPT) == SG_OBLIG:
+        if ch.get(MTRX_FRONT) == 'multi' and ch.get(MTRX_FR_OPT) == SG_OBLIG:
             mylang.add('insitu-int-cl := [ SYNSEM.LOCAL.CAT.MC - ].')
 
         # For non-free word orders, need to rule out structural ambiguity:
