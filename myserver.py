342c4c00500d071a3fa346f60a86958ff43aa636
ajcassell <41599599+ajcassell@users.noreply.github.com>
2024-08-15 15:41:20 -0700

    initial text edits and adding myserver.py

---
+++ myserver.py
@@ -0,0 +1,28 @@
+"""
+A general-purpose script for running the customization webpage locally.
+"""
+import http.server
+
+PORT = 9000
+
+# Treat everything as a cgi file, i.e.
+# `handler.cgi_directories = ["*"]` but that is not defined, so we need:
+class Handler(http.server.CGIHTTPRequestHandler):
+  def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="", **kwargs)
+  def is_cgi(self):
+    self.cgi_info = '', self.path[1:]
+    if "matrix.cgi" in self.path:
+      print("This is the self path", self.path)
+      self.headers.Cookie='session=0000'
+      return True
+    else:
+      return False
+
+with http.server.HTTPServer(("", PORT), Handler) as httpd:
+    try:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nclose server")
+        httpd.server_close()
